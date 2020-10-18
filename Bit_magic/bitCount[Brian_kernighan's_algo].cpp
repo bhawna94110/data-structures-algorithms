e713d645d2c847b92bad02a6b144fc273a2be0f4
Bhawna Rajput <bhawnarajput94110@gmail.com>
2020-10-17 15:06:46 -1100

    bitMagic

---
+++ Bit_magic/bitCount[Brian_kernighan's_algo].cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int countBit(int n)
+{
+	int res=0;
+	while(n>0)
+	{
+		n = (n & (n-1));
+		res++;
+	}
+	return res;
+}
+int main()
+{
+	int n=5;
+	cout<<countBit(n);
+	return 0;
+}
